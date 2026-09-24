7167866fb18f342ef69dfbb7cced46b7cb74d404
bhinnekakulturaindonesia-dev <bhinnekakulturaindonesia@gmail.com>
2026-09-24 13:44:27 +0700

    Add: Test page to debug routing

---
+++ app/admin/test/page.js
@@ -0,0 +1,10 @@
+export const dynamic = 'force-dynamic';
+
+export default function TestPage() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>TEST PAGE WORKS!</h1>
+      <p>If you see this, routing works fine.</p>
+    </div>
+  );
+}
