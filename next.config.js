e6857a8b05328355e533c9e66a8edfde5104f24c
bhinnekakulturaindonesia-dev <bhinnekakulturaindonesia@gmail.com>
2026-09-26 13:17:38 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,5 +7,17 @@ const nextConfig = {
       { protocol: 'https', hostname: 'kulturaindonesia.or.id' },
     ],
   },
+
+  // Redirect dari domain lama ke domain baru
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'studikulturaindonesia.vercel.app' }],
+        destination: 'https://kulturaindonesia.or.id/:path*',
+        permanent: true, // 301 redirect
+      },
+    ]
+  },
 }
 module.exports = nextConfig
